16c766630c47740c2e1363c10134fcd0206c3aab
LEONARD RICHARD <leonardrichard107@gmail.com>
2021-05-21 15:29:00 -0300

    Componentes Lista e About

---
+++ src/lista/lista.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Lista extends Component {
+    render() {
+        return(
+            <div>
+                <h1>Lista</h1>
+            </div>
+        )
+    }
+}
--- src/main/app.jsx
+++ src/main/app.jsx
@@ -2,9 +2,12 @@ import 'modules/bootstrap/dist/css/bootstrap.min.css'
 import 'modules/font-awesome/css/font-awesome.min.css'
 
 import React from 'react'
+import Lista from '../lista/lista'
+import About from '../about/about'
 
 export default props => (
     <div className="container">
-        <h1>Teste</h1>
+        <Lista />
+        <About />
     </div>   
 )
