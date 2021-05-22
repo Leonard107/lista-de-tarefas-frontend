e2de6a9ba449d22e3cd316062d8fb3dcaee042e1
LEONARD RICHARD <leonardrichard107@gmail.com>
2021-05-22 15:16:31 -0300

    Componente PageHeader

--- src/lista/lista.jsx
+++ src/lista/lista.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default class Lista extends Component {
     render() {
         return(
             <div>
-                <h1>Lista</h1>
+                <PageHeader name='Tarefas' small='Cadastro'/>
             </div>
         )
     }
