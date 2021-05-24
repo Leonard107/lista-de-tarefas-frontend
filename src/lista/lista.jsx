202656ead6e0fd9c1cb834fd17cd739e46a2f406
LEONARD RICHARD <leonardrichard107@gmail.com>
2021-05-24 07:35:37 -0300

    Componentes pendenciaForm e pendenciaList

--- src/lista/lista.jsx
+++ src/lista/lista.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
 import PageHeader from '../template/pageHeader'
+import ListaForm from './listaForm'
+import ListaLista from './listaLista'
 
 export default class Lista extends Component {
     render() {
         return(
             <div>
                 <PageHeader name='Tarefas' small='Cadastro'/>
+                <ListaForm />
+                <ListaLista />
             </div>
         )
     }
