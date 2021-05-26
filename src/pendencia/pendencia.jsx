533649d74a2fd602c43c7e92f1f1a19261c019be
LEONARD RICHARD <leonardrichard107@gmail.com>
2021-05-26 18:04:11 -0300

    Evento onChange

--- src/pendencia/pendencia.jsx
+++ src/pendencia/pendencia.jsx
@@ -6,18 +6,27 @@ import PendenciaLista from './pendenciaLista'
 export default class Pendencia extends Component {
     constructor(props) {
         super(props)
+        this.state = { description: '', list: [] }
+
+        this.handleChange = this.handleChange.bind(this)
         this.handleAdd = this.handleAdd.bind(this)   
     }
 
+    handleChange(e){
+        this.setState({...this.state, description: e.target.value })
+    }
+
     handleAdd() {
-        console.log(this)
+        console.log(this.state.description)
     }
 
     render() {
         return(
             <div>
                 <PageHeader name='Tarefas' small='Cadastro'/>
-                <PendenciaForm handleAdd={this.handleAdd}/>
+                <PendenciaForm description={this.state.description}
+                    handleChange={this.handleChange}
+                    handleAdd={this.handleAdd}/>
                 <PendenciaLista />
             </div>
         )
--- src/pendencia/pendenciaForm.jsx
+++ src/pendencia/pendenciaForm.jsx
@@ -6,7 +6,9 @@ export default props => (
     <div role='form' className='pendenciaForm'>
         <div className='col-xs-12 col-sm-9 col-md-10'>
             <input id='description' className='form-control' 
-            placeholder="Adicione uma tarefa"></input>
+            placeholder='Adicione uma tarefa'
+             onChange={props.handleChange}
+            value={props.description}></input>
         </div>
 
         <div className='col-xs-12 col-sm-9 col-md-2'>
