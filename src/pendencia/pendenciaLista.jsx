2bd4b588ff7a0043ccb54a79eb6e665ab89208bd
LEONARD RICHARD <leonardrichard107@gmail.com>
2021-05-28 07:57:58 -0300

    Consulta e Exclusão de pendencias

--- src/pendencia/pendenciaLista.jsx
+++ src/pendencia/pendenciaLista.jsx
@@ -1,7 +1,32 @@
 import React from 'react'
+import IconButton from '../template/iconButton'
+export default props => {
+  
+  const renderRows = () => {
+    const list = props.list || []
+    return list.map(lista =>(
+      <tr key={lista._id}>
+        <td>{lista.description}</td>
+        <td>
+          <IconButton style='danger' icon='trash-o'
+            onClick={() => props.handleRemove(lista)}
+          />
+        </td>
+      </tr>
+    ))
+  }
 
-export default props => (
-  <div>
-      <h2>Lista</h2>
-  </div>
-)
+  return (
+    <table className='table'>
+      <thead>
+        <tr>
+          <th>Descrição</th>
+          <th>Ações</th>
+        </tr>
+      </thead>
+      <tbody>
+        {renderRows()}
+      </tbody>
+    </table>
+  )
+}
